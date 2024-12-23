26e75e3b1ab7d4c40d2561d6a6206bf2075f0231
Jancy11 <jancynithya.j@gmail.com>
2024-12-23 23:49:42 +0530

    Update model.py
    new feature

--- app/demand_forecasting/model.py
+++ app/demand_forecasting/model.py
@@ -3,7 +3,7 @@ from statsmodels.tsa.arima.model import ARIMA
 
 def forecast_sales(data_path, steps=30):
     data = pd.read_csv(data_path, parse_dates=['date'], index_col='date')
-    model = ARIMA(data['sales'], order=(5, 1, 0))  # Adjust order for your dataset
+    model = ARIMA(data['sales'], order=(5, 1, 0))  
     model_fit = model.fit()
     forecast = model_fit.forecast(steps=steps)
     return forecast
